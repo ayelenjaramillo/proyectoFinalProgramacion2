87842d60bf6b44d83fae301d5c8589d3966e2cc9
Ayelen Jaramillo <ayelenjaramillotw@gmail.com>
2026-06-24 20:16:12 -0300

    modifico database

--- database/config/database.js
+++ database/config/database.js
@@ -1,34 +1,29 @@
 const mysql2 = require('mysql2');
+require('dotenv').config();
 
-require("dotenv").config();
-
-module.exports={
+module.exports = {
   development: {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT || 3307,
-    dialect: "mysql", 
+    dialect: 'mysql',
     dialectModule: mysql2,
     logging: false
-  }, 
+  },
   production: {
-    username: process.env.PROD_DB_USER,
-    password: process.env.PROD_DB_PASS,
-    database: process.env.PROD_DB_NAME,
-    host: process.env.PROD_DB_HOST,
-    port: process.env.PROD_DB_PORT,
-    dialect: "mysql", 
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'mysql',
     dialectModule: mysql2,
-    logging: false, 
-    dialectOptions:{
-      ssl:{
+    logging: false,
+    dialectOptions: {
+      ssl: {
         rejectUnauthorized: false
       }
-    } 
+    }
   }
-}
+};
 
 
 //Applications/MAMP/Library/bin/mysql57/bin/mysql
